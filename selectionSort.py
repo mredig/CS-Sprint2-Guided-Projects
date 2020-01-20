1054ebb2f87d8b8bee61153f19598b2b71ab62c9
Michael Redig <mredig@gmail.com>
2020-01-19 18:53:56 -0600

    updated selection sort

--- selectionSort.py
+++ selectionSort.py
@@ -3,11 +3,6 @@ myList = [8, 6, 2, 3, 7, 9, 5, 0, 1, 4]
 
 
 def selectionSort(items):
-    # for each element in the array
-        # compare it to each element to the right
-        # track the lowest value
-        # whichever was lowest, swap into current evaluator
-
     for i in range(len(items)):
         lowestIndex = i
         j = i
